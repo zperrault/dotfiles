45b90fe4422e8ff8717e604f9e859cb8911278f9
Zach Perrault <zach@perrault.email>
2020-03-21 00:07:15 -0400

    install `z`; add colorscheme

--- configs/zsh/rc/03.extensions-and-programs.zsh
+++ configs/zsh/rc/03.extensions-and-programs.zsh
@@ -8,10 +8,3 @@ fi
 if hash rbenv; then
   eval "$(rbenv init -)"
 fi
-
-BASE16_SHELL="$HOME/.config/base16-shell/"
-[ -n "$PS1" ] && \
-    [ -s "$BASE16_SHELL/profile_helper.sh" ] && \
-        eval "$("$BASE16_SHELL/profile_helper.sh")"
-
-base16_onedark
